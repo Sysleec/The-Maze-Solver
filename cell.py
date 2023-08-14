bc9764c237fe02863d4ecd6d85b5bb2e1fbc1e74
sysleec <sysleec@gmail.com>
2023-08-14 14:20:07 +0300

    break the walls

--- cell.py
+++ cell.py
@@ -12,6 +12,7 @@ class Cell:
         self._y1 = False
         self._y2 = False
         self._win = win
+        self.visited = False
 
     def draw(self, x1, y1, x2, y2):
         self.x1 = x1
@@ -21,15 +22,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "black")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "black")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "black")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "black")
 
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
